b3048c531bfbc3b280bd54073a605121e7978691
MeetPatel5 <meet.patel@openxcellinc.com>
2018-10-01 11:03:17 +0530

    forth commit

--- Basic/read.sql
+++ Basic/read.sql
@@ -2,3 +2,41 @@
 
 SELECT *
 FROM students;
+
+-- By Not Sprecifing Ref of Table for column
+SELECT name
+FROM students
+
+-- By Refering a students table
+SELECT students.student_id, students.name
+FROM students
+
+
+
+/*
+    CHECK GRP OF VALUES
+*/
+
+SELECT *
+FROM students
+WHERE name IN ("Meet","Shubham","Krishna")
+
+SELECT *
+FROM students
+WHERE name IN ("Meet","Shubham","Krishna") AND students_id > 2
+
+/*
+    ORDER_BY
+*/
+
+SELECT *
+FROM students
+ORDER BY major, student_id DESC
+LIMIT 2
+
+/*
+    OPERATORS 
+*/
+
+-- > < , <= >= , = ,AND ,OR , <> (!=)
+
--- Basic/update.sql
+++ Basic/update.sql
@@ -5,3 +5,8 @@ ALTER TABLE students ADD gpa DECIMAL(3,2)
 
 -- Droping a table is also a altering a table 
 ALTER TABLE student DROP COLUMN gap
+
+-- Upate specific column
+UPDATE students
+SET major = "COMP"
+WHERE major = "MECH" OR major="ELEC"
